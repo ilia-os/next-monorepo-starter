c6527640c8819e9710f7cbeb5a3964a8691c29da
Ilia Os <dev.osipov@gmail.com>
2022-05-10 18:42:54 +0400

    chore(frontend): add hygen generators

---
+++ apps/frontend/_templates/generate/component/prompt.js
@@ -0,0 +1,24 @@
+module.exports = [
+	{
+		type: 'input',
+		name: 'name',
+		message: 'Type the name of the component',
+	},
+	{
+		type: 'select',
+		name: 'folder',
+		message: 'Select component folder...',
+		choices: [
+      'landing',
+      'common',
+      'layout',
+      'utility',
+      'typography',
+      'approach',
+      'about',
+      'insights',
+      'services',
+      'methodologies'
+    ],
+	},
+]
---
+++ apps/frontend/_templates/generate/helper/export.ejs
@@ -0,0 +1,6 @@
+---
+inject: true
+to: src/helpers/index.ts
+append: true
+---
+export { default as <%= name %> } from './<%= name %>'

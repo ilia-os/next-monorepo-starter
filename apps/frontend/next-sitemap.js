4dfbee11dda3c44bac61ae1c30282a7b61686d4f
Ilia Os <dev.osipov@gmail.com>
2022-05-30 14:18:03 +0400

    chore(frontend): restrict robots

--- apps/frontend/next-sitemap.js
+++ apps/frontend/next-sitemap.js
@@ -2,4 +2,12 @@
 module.exports = {
 	siteUrl: process.env.APP_URL || 'http://localhost',
 	generateRobotsTxt: true,
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: '*',
+				disallow: '*',
+			},
+		],
+	},
 }

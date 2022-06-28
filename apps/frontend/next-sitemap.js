57322c87556a80ce272785155e66c1fb95515b2c
Ilia Os <dev.osipov@gmail.com>
2022-06-28 14:55:19 +0400

    fix(frontend): fix APP_URL env name

--- apps/frontend/next-sitemap.js
+++ apps/frontend/next-sitemap.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.NEX_PUBLIC_APP_URL || 'http://localhost',
+	siteUrl: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost',
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
 		policies: [
